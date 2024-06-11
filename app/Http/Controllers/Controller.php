d28915e00363079e3cc0a9ab0e16a12a5c1993a8
Arsentiy Zhunussov <arsentiy.zhunussov@gmail.com>
2024-06-11 17:57:41 +0500

    [PRE] Add base api controllers and swagger documentation on #2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,29 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *      title="blog.localhost",
+ *      version="1.0",
+ *      description="",
+ *
+ *      @OA\Contact(
+ *          email="arsentiy.zhunussov@gmail.com"
+ *      ),
+ *
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
+abstract class Controller extends BaseController
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
